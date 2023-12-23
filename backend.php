7441d3535bd84cbbca97764b356a668b39862461
sunitabashyal <snizabashyal@gmail.com>
2023-12-23 09:24:35 +0545

    data from backend

---
+++ backend.php
@@ -0,0 +1,9 @@
+<?php
+
+$response = array(
+		"question" => "How old are you ?", 
+		"options" => ["I am 20", "I am 25", "I am 26", "I am 27"]
+	);
+$json_response = json_encode($response)
+
+?>
--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
 
+require "backend.php";
+$data = json_decode($json_response, true);
+
 $page_header = "Welcome to Quiz Program";
 echo "<h2>$page_header</h2>";
 
 
-
-
-function question(){
-	$question_text = "How old are you ?";
-	return "<p>" . $question_text . "</p>";
+function get_question($question){
+	return "<p>" . $question . "</p>";
 }
 
-function options(){
-	$option_items = ["I am 20", "I am 25", "I am 26", "I am 27"];
+function get_options($options){
 	$rendered_options = "";
 	
-	for ($i = 0; $i < count($option_items); $i++) {
+	for ($i = 0; $i < count($options); $i++) {
     	$rendered_options = $rendered_options . "<p>" .
     		'<input type="radio" name="color" value="red" id="red">
-        <label for="red">' . $option_items[$i] . '</label>'
+        <label for="red">' . $options[$i] . '</label>'
     	  . "</p> ";
 	}
 	return $rendered_options;
@@ -29,8 +28,8 @@ function submit_button(){
 	return "<button>Submit</button>";
 
 }
-echo question();
-echo options();
+echo get_question($data["question"]);
+echo get_options($data["options"]);
 echo submit_button();
-?>
 
+?>
