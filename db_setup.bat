502cb3485639c296a083b1ef1e5b7deb9fc15095
sunitabashyal <snizabashyal@gmail.com>
2023-12-27 14:01:30 +0545

    code refactor

--- db_setup.bat
+++ db_setup.bat
@@ -33,14 +33,14 @@ REM ----------------------------------------------------------------------------
 set quiz_query_cmd=%MYSQL_BIN_PATH%\mysql.exe -h %mysql_host% -u %MYSQL_USER% -D %MYSQL_DATABASE%
 
 REM Set database and table names
-set question_table_name=question
+set table_name=question
 
 REM Create the questions table
-set create_question_table_query="CREATE TABLE IF NOT EXISTS %question_table_name% ( id INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT NOT NULL,  option_a VARCHAR(255) NOT NULL,  option_b VARCHAR(255) NOT NULL, option_c VARCHAR(255) NOT NULL, option_d VARCHAR(255) NOT NULL, correct_option VARCHAR(1) NOT NULL );"
+set create_table_query="CREATE TABLE IF NOT EXISTS %table_name% ( id INT PRIMARY KEY AUTO_INCREMENT, question_text TEXT NOT NULL,  option_a VARCHAR(255) NOT NULL,  option_b VARCHAR(255) NOT NULL, option_c VARCHAR(255) NOT NULL, option_d VARCHAR(255) NOT NULL, correct_option VARCHAR(1) NOT NULL );"
 
-set create_table_cmd=%quiz_query_cmd% -e %create_question_table_query%
+set create_table_cmd=%quiz_query_cmd% -e %create_table_query%
 
 :: Run the MySQL command
 %create_table_cmd%
-echo Table %question_table_name% created successfully.............
+echo Table %table_name% created successfully.............
 REM ------------------------------------------------------------------------------------------------------------------
